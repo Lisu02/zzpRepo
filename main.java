8c787c39ef74811ca7ba276b303525d18eac47e2
Lisu02 <Lisu02@github.com>
2024-04-16 12:02:38 +0200

    Super nowy brancz

--- main.java
+++ main.java
@@ -2,8 +2,9 @@
 public class Main{
 
 	public static void main(String args[]){
-	System.out.println(System.currentTimeMillis());
-
+		System.out.println(System.currentTimeMillis());
+	//SUPER BRANCZ
+//123
 	}
 
 }
